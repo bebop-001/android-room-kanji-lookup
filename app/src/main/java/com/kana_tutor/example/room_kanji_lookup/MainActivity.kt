9d44b6142254e7ee269357da42db005fc9a06675
sjs <sjs@sonic.net>
2020-07-12 11:23:20 -0700

    Initial

---
+++ app/src/main/java/com/kana_tutor/example/room_kanji_lookup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kana_tutor.example.room_kanji_lookup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
